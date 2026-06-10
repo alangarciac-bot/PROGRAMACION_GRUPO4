827b415ac186a5fb211a4845e3b29e3facb19490
anfelamaru <anfel.amaru@ucb.edu.bo>
2026-06-10 13:25:44 -0400

    Update Julio.cpp

--- MiniProyecto/Julio.cpp
+++ MiniProyecto/Julio.cpp
@@ -1,3 +1,13 @@
+// Materia: Programación I, Paralelo 4
+
+// Grupo: 4
+
+// Autor:  Julio Angel Rodriguez Marca
+
+// Fecha creación: 09/06/2026
+
+// Nombre del Miniproyecto: Sistema de Habilitación UCB
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
