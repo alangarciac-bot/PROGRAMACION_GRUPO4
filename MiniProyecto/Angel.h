6e61fad88a51fc61a815f0416a97e70d7cd9e8fa
anfelamaru <anfel.amaru@ucb.edu.bo>
2026-06-10 13:21:48 -0400

    Update Angel.h

--- MiniProyecto/Angel.h
+++ MiniProyecto/Angel.h
@@ -1,3 +1,13 @@
+// Materia: Programación I, Paralelo 4
+
+// Grupo: 4
+
+// Autor: Angel José Amaru Llojlla 
+
+// Fecha creación: 08/06/2026
+
+// Nombre del Miniproyecto: Sistema de Habilitación UCB
+
 #ifndef ANGEL_H
 #define ANGEL_H
 void generarReporteHabilitacion();
