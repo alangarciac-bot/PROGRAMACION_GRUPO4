6516b06ce5354316ac9190ea4001d24840eb02b5
anfelamaru <anfel.amaru@ucb.edu.bo>
2026-06-10 13:13:17 -0400

    Update Angel.cpp

--- MiniProyecto/Angel.cpp
+++ MiniProyecto/Angel.cpp
@@ -1,3 +1,13 @@
+// Materia: Programación I, Paralelo 4
+
+// Grupo: 4
+
+// Autor: Angel José Amaru Llojlla 
+
+// Fecha creación: 08/06/2026
+
+// Nombre del Miniproyecto: Sistema de Habilitación UCB
+
 #include <iostream>
 #include <fstream>
 #include <vector>
