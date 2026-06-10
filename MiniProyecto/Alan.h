7e0e38ea9de871cc09958957fe6e707465f34443
anfelamaru <anfel.amaru@ucb.edu.bo>
2026-06-10 13:28:13 -0400

    Update Alan.h

--- MiniProyecto/Alan.h
+++ MiniProyecto/Alan.h
@@ -1,3 +1,13 @@
+// Materia: Programación I, Paralelo 4
+
+// Grupo: 4
+
+// Autor: Alan Samir Garcia Condori  
+
+// Fecha creación: 09/06/2026
+
+// Nombre del Miniproyecto: Sistema de Habilitación UCB
+
 #ifndef ALAN_H
 #define ALAN_H
 void eliminarEstudiante();
